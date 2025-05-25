7543e0b17bafc4adfa6304061d7e2a8aa849253c
PotatOverlord <160378192+PotatOverlord@users.noreply.github.com>
2025-05-25 05:33:02 +0100

    Im garfielding it

--- profiles/cake/configuration.nix
+++
@@ -1,9 +0,0 @@
-{ userSettings, ... }:
-
-{
-  imports = [ ./base.nix
-              ( import ../../system/security/sshd.nix {
-                authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIK4fRchvTJr3nI9pM4t22Uv7K2pGkSglMViZHkVvUBN tater@cheese"];
-                inherit userSettings; })
-            ];
-}
